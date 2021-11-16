7f9c8b447b9187a51c61ed05d409ae9fb2f93dad
seenso <>
2021-11-16 10:55:36 -0900

    Backup

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This project will be based around nutrition and fruits specifically and the various information that can be obtained from the api. 
 So for this project, we’ll Include fruit infomation such as calories, what its name is, what Family and Genus it belongs to among other things. 
 
-The challenges that might come up are Unexpected errors and having to work through that, along with bringing together all of the functionality of the site and making it work. 
+The challenges that might⁄ come up are Unexpected errors and having to work through that, along with bringing together all of the functionality of the site and making it work. 
 
 Core features of this site are that once a button is clicked, it would indicate that I liked the fruit and save it.
 Also, you can click a button and indicate that you do not like that specific fruit and want it to be removed from the page. 
--- script.js
+++ script.js
@@ -1,7 +1,62 @@
-const url = "https://www.fruityvice.com/api/fruit/all";
+const url = "http://localhost:3000/fruitData";
+// data in db.json is originally from https://www.fruityvice.com/api/fruit/all
+const fruitCardContainer = document.querySelector("div#fruitCardContainer");
 
-fetch(url)
-.then(res => res.json())
-.then(fruitData => {
-  console.log("FRUIT DATA", fruitData);
-});
+window.addEventListener("DOMContentLoaded", () => {
+  //GET FRUIT DATA
+  fetch(url)
+  .then(res => res.json())
+  .then(fruitData => {
+    fruitData.forEach(fruit => { //fruit is the fruit obj with name, genus, nutrition, etc.
+      renderFilterBar(fruit);
+      renderFruitCards(fruit);
+    })
+  })
+  .catch(error => {
+    console.log("ERROR", error);
+  });
+})
+
+function renderFilterBar(fruit) {};
+
+function renderFruitCards(fruit) {
+  const fruitCardContainer = document.querySelector("div#fruitCardContainer");
+  //One fruit Card
+  const fruitCard = document.createElement("div");
+    fruitCard.id = "fruitCard";
+  //Fruit Name
+  const fruitName = document.createElement("h2");
+    fruitName.textContent = fruit.name;
+    fruitCard.append(fruitName);
+  //Fruit Nutrition
+  const nutrition = document.createElement("div");
+    //Calories, Carbohydrates, Sugar, Protein, & Fats
+    nutrition.innerHTML = `
+      <b>Nutrition</b><br>
+      Calories: ${fruit.nutritions.calories}<br>
+      Carbohydrates: ${fruit.nutritions.carbohydrates}<br>
+      Sugar: ${fruit.nutritions.sugar}<br>
+      Protein: ${fruit.nutritions.protein}<br>
+      Fats: ${fruit.nutritions.fat}<br>
+    `;
+    fruitCard.append(nutrition);
+
+  //Like/Delete Fruit
+    //created heart icon
+
+      //add event listener
+      // heartIconName.addEventListener("click", (event) => {
+      //   event.preventDefault();
+      //   //thing to do when heart icon is clicked.
+        
+      // });
+
+
+
+
+    //append to 
+    // fruitCard.append();
+
+  //end
+  fruitCardContainer.append(fruitCard);
+};
